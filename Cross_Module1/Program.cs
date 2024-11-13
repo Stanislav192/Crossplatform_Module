83c6e23655b4b0dfa84e3f8f9327cc5379767fa1
Yuk1ra <159479985+Stanislav192@users.noreply.github.com>
2024-11-13 09:41:40 +0200

    Added_2_methods

---
+++ Cross_Module1/Program.cs
@@ -0,0 +1,107 @@
+﻿using System.Text;
+namespace Cross_Module1
+{
+    public class Program
+    {
+        public static string Swap(string a, int i, int j)
+        {
+            try
+            {
+                //Перевірка, чи наші індекси в межах допустимого діапазону
+                if (i < 0 || j < 0 || i >= a.Length || j >= a.Length)
+                {
+                    throw new IndexOutOfRangeException("Indexes should be within the length of the string.");
+                }
+
+                char[] charArr = a.ToCharArray();
+
+                char temp = charArr[i];
+
+                charArr[i] = charArr[j];
+
+                charArr[j] = temp;
+
+                return new string(charArr);
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unknown Error: {ex.Message}");
+                return a;
+            }
+        }
+
+        public static void Rearrange(string text, int indx, StringBuilder ress)
+        {
+
+            if (indx < 0 || indx >= text.Length)
+            {
+                Console.WriteLine("The index is not within acceptable limits");
+            }
+
+            if (indx == text.Length - 1)
+            {
+                ress.AppendLine(text);
+                return;
+            }
+
+            for (int i = indx; i < text.Length; i++)
+            {
+
+                text = Swap(text, indx, i);
+
+                Rearrange(text, indx + 1, ress);
+
+                //Повертаємо рядок до попереднього стану
+                text = Swap(text, indx, i);
+
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                string inputFile = Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "..", "Cross_Module1", "INPUT.txt").Trim();
+
+                if (!File.Exists(inputFile))
+                {
+                    Console.WriteLine($"File {inputFile} wasn't found");
+                    return;
+                }
+
+
+                string inputStr = File.ReadAllText(inputFile);
+
+                if (string.IsNullOrEmpty(inputStr))
+                {
+                    Console.WriteLine("Error.String cannot be empty");
+                    return;
+                }
+
+                if (!IsStringOnlyLetters(inputStr))
+                {
+                    Console.WriteLine("Error. The input must contain only letters.");
+                    return;
+                }
+
+                StringBuilder result = new StringBuilder();
+
+                Rearrange(inputStr, 0, result);
+
+                File.WriteAllText(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "..", "Cross_Module1", "OUTPUT.txt"), result.ToString().Trim());
+
+                Console.WriteLine($"The results are recorded in OUTPUT.TXT");
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unknown error: {ex.Message}");
+            }
+        }
+        public static bool IsStringOnlyLetters(string str)
+        {
+            return System.Text.RegularExpressions.Regex.IsMatch(str, @"^[a-zA-Z]+$");
+        }
+    }
+}
